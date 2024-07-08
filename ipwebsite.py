dc376f59cc7c3d03fa7f31bd5713caaaf917f9db
hamosa123 <mosamosamoh123@gmail.com>
2024-07-08 11:49:15 +0300

    Add files via upload

---
+++ ipwebsite.py
@@ -0,0 +1,9 @@
+import socket
+import sys
+import os
+os.system('clear')
+print('type link website Ex: www.facebook.com:') 
+name = input('»»»»» ') 
+ip=socket.gethostbyname(name)
+print('ip website is:»»» '+ ip) 
+print(" 👽cobra elyoutuber👽 LOVE YOU♥") 
